5cbc0d0c4ec23e0d3dc13ce3403b7f5ca632675a
변성일 <127079075+byunsungil@users.noreply.github.com>
2025-05-07 17:37:36 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SELECT 
+    AO.ANIMAL_ID, 
+    AO.NAME
+FROM ANIMAL_OUTS AO
+JOIN ANIMAL_INS AI ON AO.ANIMAL_ID = AI.ANIMAL_ID
+ORDER BY TIMESTAMPDIFF(SECOND, AI.DATETIME, AO.DATETIME) DESC
+LIMIT 2;
+
+
+
+# 입양을 간 동물 중, 보호 기간이 가장 길었던 동물 두 마리의 아이디와 이름을 조회
