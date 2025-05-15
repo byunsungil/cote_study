eee7b591fade65dfc4f39f9ed2a2ea032fb7a0bc
변성일 <127079075+byunsungil@users.noreply.github.com>
2025-05-15 13:29:58 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,18 @@
+-- 코드를 작성해주세요
+SELECT
+  D.ID,
+  D.EMAIL,
+  D.FIRST_NAME,
+  D.LAST_NAME
+FROM
+  DEVELOPERS D
+WHERE
+  EXISTS (
+    SELECT 1
+    FROM SKILLCODES S
+    WHERE S.NAME IN ('Python', 'C#')
+      AND (D.SKILL_CODE & S.CODE) > 0
+  )
+ORDER BY
+  D.ID ASC;
+
